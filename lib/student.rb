f9816f3a0d50786a940b30509f92956077333f56
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-10 08:09:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,6 +15,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       student = self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
